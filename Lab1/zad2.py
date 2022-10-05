9b6d313f69ed380cf8ec69e52e7a00b7a6b5d6a4
Grazyna <gszostek@wsiz.edu.pl>
2022-10-05 20:41:14 +0200

    dodano plik zad2

---
+++ Lab1/zad2.py
@@ -0,0 +1,15 @@
+zm1 = int(6.5)
+print(zm1)
+print(type(zm1))
+
+zm2 = int(3.0)
+print(zm2)
+print(type(zm2))
+
+zm3 = float(3)
+print(zm3)
+print(type(zm3))
+
+zm4 = float("3")
+print(zm4)
+print(type(zm4))

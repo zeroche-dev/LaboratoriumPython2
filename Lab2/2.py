8f34b12ac60f369ba015e103099b5c3156386135
gszostek1 <115081244+gszostek1@users.noreply.github.com>
2022-10-16 15:37:45 +0200

    Delete Lab2 directory

--- Lab2/2.py
+++
@@ -1,9 +0,0 @@
-#--------------------zad.2.2-------------------------------------------------
-import random
-droga = random.randint(1,1000)
-
-#droga =float(input('Podaj długość drogi przejechanej przez samochód (w km): '))
-print(f'Długość drogi przejechanej przez samochód (w km): {droga}')
-spal = float(input('Podaj średnie spalanie (w l/100km): '))
-print(f"Przywidywane zużycie paliwa: {droga / 100 * spal} l")
-print(f"Szacowany koszt podróży: {droga*spal/100*6.5} zł")
--- Lab2/4.py
+++
@@ -1,8 +0,0 @@
-znak = input('Wprowadź znak: ')
-
-if znak >= 'a' and znak <= 'z':
-    print('Wprowadzono małą literę.')
-elif znak >= 'A' and znak <= 'Z':
-    print('Wprowadzono dużą literę.')
-else:
-    print('Wprowadzony znak nie jest literą.')

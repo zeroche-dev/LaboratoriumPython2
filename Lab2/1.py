5ac8d8b1cdd8a3120f0a0ea54bcd530cf788f1aa
gszostek1 <gszostek@wsiz.edu.pl>
2022-10-20 15:07:50 +0200

    Rozwiązanie zadań z Lab2 (zad.1-3)

---
+++ Lab2/1.py
@@ -0,0 +1,6 @@
+# Dla osób poniżej 4 roku życia wstęp jest bezpłatny.
+# • Dla osób w wieku od 4 do 18 lat bilet kosztuje 10zł.
+# • Dla osób powyżej 18 roku życia bilet kosztuje 20zł.
+# Przykład: Wprowadź wiek klienta: 5
+# Cena biletu: 10zł
+

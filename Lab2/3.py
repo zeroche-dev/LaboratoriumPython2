0c88340e0e81409d38422d5a424947f46f48450b
gszostek1 <gszostek@wsiz.edu.pl>
2022-10-23 11:23:50 +0200

    Rozwiązania zadań z Lab2

---
+++ Lab2/3.py
@@ -0,0 +1,6 @@
+print('''Jaką operację chcesz wykonać?
+1) dodawanie
+2) odejmowanie
+3) mnożenie
+4) dzielenie
+5) potęgowanie''')
---
+++ Lab2/lab3_1.py
@@ -0,0 +1,2 @@
+# Napisz skrypt, który pobierze od użytkownika dwie liczby całkowite. Następnie zaczynając od
+# mniejszej liczby, wypisze kolejne liczby aż do osiągnięcia wartości drugiej (większej) liczby.

3c84486bc53c4e1a601bb3cc50cfd1fc0801e9e8
gszostek1 <115081244+gszostek1@users.noreply.github.com>
2022-10-24 07:48:07 +0200

    Delete Lab2 directory

--- Lab2/3.py
+++
@@ -1,6 +0,0 @@
-print('''Jaką operację chcesz wykonać?
-1) dodawanie
-2) odejmowanie
-3) mnożenie
-4) dzielenie
-5) potęgowanie''')
--- Lab2/lab3_1.py
+++
@@ -1,2 +0,0 @@
-# Napisz skrypt, który pobierze od użytkownika dwie liczby całkowite. Następnie zaczynając od
-# mniejszej liczby, wypisze kolejne liczby aż do osiągnięcia wartości drugiej (większej) liczby.

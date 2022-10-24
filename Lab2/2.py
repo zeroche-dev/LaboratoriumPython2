74694feb2db09186ca001275018b46d9f4b31dcd
gszostek1 <115081244+gszostek1@users.noreply.github.com>
2022-10-24 09:53:42 +0200

    Delete Lab2 directory

--- Lab2/2.py
+++
@@ -1,4 +0,0 @@
-# Napisz skrypt, który pobiera od użytkownika drogę pokonaną przez samochód oraz średnie
-# spalanie (w litrach na 100 km) i wyświetli informację o przewidywanym zużyciu paliwa oraz o szacowanych
-# kosztach podróży (cena paliwa 6.5 zł/l).
-
